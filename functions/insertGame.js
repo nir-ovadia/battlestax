6d0bb99b10ae2dd9a0a556d681a921a9cb0a5b9a
nir ovadia <nir.ovadia@datastax.com>
2023-08-03 20:27:33 +0000

    Merging insertGame and insertGame.test.js into master

--- functions/insertGame.js
+++ functions/insertGame.js
@@ -4,16 +4,27 @@ exports.handler = async (event, context) => {
   let gameId;
   let gamePayload;
   try {
-    // let's set the game id
-    // let's parse the incoming payload
+// let's set the game id
+gameId = event.path.split("insertGame/")[1];
+// let's parse the incoming payload
+gamePayload = JSON.parse(event.body);
   } catch (e) {
     // let's return a 400 if we can't parse the game id or body
+ return {
+  statusCode: 400,
+  body: JSON.stringify({ message: "must provide a valid game ID" }),
+};
   }
 
   // let's connect to Astra
   const astraClient = await createClient({
-    // let's set our Astra connection configuration
+  astraDatabaseId: process.env.ASTRA_DB_ID,
+  astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,	  
   });
+
+
+	  // let's set our Astra connection configuration
 
   const gamesCollection = astraClient
     .namespace(process.env.ASTRA_DB_KEYSPACE)
@@ -22,9 +33,19 @@ exports.handler = async (event, context) => {
   // let's provision a new game
   try {
     // let's create a new game with the gamesCollection
+    const res = await gamesCollection.create(gameId, gamePayload);
+
     // let's return a 200 with the resoponse from astra
+    return {
+      statusCode: 200,
+      body: JSON.stringify(res),
+    };
   } catch (e) {
     console.error(e);
     // let's return a 500 on error
+    return {
+      statusCode: 500,
+      body: JSON.stringify(e),
+    };  
   }
 };
